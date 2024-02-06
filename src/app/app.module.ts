e9391e5c6e22befa028966fecc00123bfa516a17
FelipeMolinaa <felipemolina.contato@gmail.com>
2024-02-05 21:03:24 -0300

    [feat] politica de privacidade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes, provideRouter } from '@angular/router';
+import { PoliticaComponent } from './politica/politica.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+    { path: "", component: HomeComponent },
+    { path: "politica", component: PoliticaComponent }
+]
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(routes)
+    ],
+    exports: [RouterModule],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
